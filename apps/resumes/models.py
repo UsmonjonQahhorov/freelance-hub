c7da234635ba2550fc24bb22feba3c4f7e470fd8
muhammadnur <muhammadnurpdp@gmail.com>
2023-10-29 11:00:16 +0500

    Added Resume's crud

--- apps/resumes/models.py
+++ apps/resumes/models.py
@@ -1,20 +1,24 @@
 from apps.resumes.choices import ResumeStatusChoice
-from apps.users.models import TimeStampedModel
 from django.db import models
 
+from apps.users.models import User
 
-class Resumes(TimeStampedModel):
-    owner = models.ForeignKey(to="Users.id", on_delete=models.CASCADE)
-    profession = models.ForeignKey(to="Professions.id", on_delete=models.PROTECT)
+
+class Resumes(models.Model):
+    owner = models.OneToOneField(to=User, on_delete=models.PROTECT)
     location = models.CharField(max_length=255)
     education = models.JSONField
-    status = models.CharField(choices=ResumeStatusChoice)
-    skills = models.ManyToManyField(Skills, through="ResumeSkills")
-    vacancies = models.ManyToManyField(Vacancies, through="VacancyResume")
+    status = models.CharField(choices=ResumeStatusChoice.choices)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # profession = models.OneToOneField(to=Professions, on_delete=models.PROTECT)
+    # skills = models.ManyToManyField(Skills, through="ResumeSkills")
+    # vacancies = models.ManyToManyField(Vacancies, through="VacancyResume")
 
     class Meta:
         verbose_name = "Resume"
         verbose_name_plural = "Resumes"
 
     def __str__(self):
-        return f"{self.profession}"
+        return f"{self.location}"
--- apps/resumes/serializers.py
+++ apps/resumes/serializers.py
@@ -9,10 +9,19 @@ class ResumesSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+from rest_framework import serializers
+
+
 class ResumesCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Resumes
         exclude = ['owner']
+
+    def save(self, **kwargs):
+        owner = self.context['request'].user
+        instance = Resumes(owner=owner, **self.validated_data)
+        instance.save()
+        return instance
 
 
 class ResumesDetailSerializer(serializers.ModelSerializer):
@@ -24,4 +33,5 @@ class ResumesDetailSerializer(serializers.ModelSerializer):
 class ResumesListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Resumes
-        exclude = ['created_at', 'updated_at', 'skills', 'vacancies']
+        # exclude = ['created_at', 'updated_at', 'skills', 'vacancies']
+        exclude = ['created_at', 'updated_at']
